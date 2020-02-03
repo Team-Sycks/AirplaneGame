72c46fad1d2bb154ea5e9126e8c38adaaa56721b
Sami Jaanu <jaanusa.sjrgmail.com>
2020-02-03 14:37:13 +0200

    kamera asetukset tehty

--- Assets/Codes/RTSCameraMovement.cs
+++ Assets/Codes/RTSCameraMovement.cs
@@ -4,6 +4,11 @@ public class RTSCameraMovement : MonoBehaviour
 {
     public float panSpeed = 100f;
     public float panBorderThickness = 10f;
+    public Vector2 panLimit;
+
+    public float scrollSpeed = 20f;
+    public float minY = 20f;
+    public float maxY = 120f;
 
     // Update is called once per frame
     void Update()
@@ -26,6 +31,13 @@ public class RTSCameraMovement : MonoBehaviour
         {
             pos.x -= panSpeed * Time.deltaTime;
         }
+
+       float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y += scroll * scrollSpeed * 100f * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
 
         transform.position = pos;
     }
