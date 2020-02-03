bfc0920d8ae7d25f58ff4f4823b56b5db06ded51
sandels97 <santtu.hyvarine@gmail.com>
2020-02-03 13:02:46 +0200

    Updated plane REST api fetch

--- Assets/Codes/OpenSkyNetwork.cs
+++ Assets/Codes/OpenSkyNetwork.cs
@@ -20,14 +20,45 @@ public class OpenSkyNetwork : MonoBehaviour
         
     }
 
-    private string GetAirPlane()
+    private List<Plane> GetAirPlane()
     {
         HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format(api));
         HttpWebResponse response = (HttpWebResponse)request.GetResponse();
         StreamReader reader = new StreamReader(response.GetResponseStream());
         string jsonResponse = reader.ReadToEnd();
         Debug.Log(jsonResponse);
-        
-        return jsonResponse;
+
+        PlaneList planeList = JsonUtility.FromJson<PlaneList>("{\"planes\":" + jsonResponse + "}");
+
+        //Remove planes that don't have Departure or Arrival airport listed
+        List<Plane> planes = planeList.planes;
+        for(int i = planes.Count - 1; i > -1; i--)
+        {
+            Plane plane = planes[i];
+            if(plane.estArrivalAirport.Equals("") || plane.estDepartureAirport.Equals(""))
+            {
+                planes.RemoveAt(i);
+            }
+        }
+
+        foreach (Plane plane in planes)
+        {
+            Debug.Log(plane.firstSeen + ", " + plane.lastSeen +  ", " +plane.estDepartureAirport + ", " + plane.estArrivalAirport);
+        }
+        return planes;
+    }
+
+    [Serializable]
+    public class PlaneList
+    {
+        public List<Plane> planes;
+    }
+    [Serializable]
+    public class Plane
+    {
+        public int firstSeen;
+        public int lastSeen;
+        public String estDepartureAirport;
+        public String estArrivalAirport;
     }
 }
