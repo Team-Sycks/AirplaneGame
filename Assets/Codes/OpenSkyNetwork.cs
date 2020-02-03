e61e0d9aba3ad31b44b81b2db8b867a523bd35b7
sandels97 <santtu.hyvarine@gmail.com>
2020-02-03 12:06:30 +0200

    Rest api test

---
+++ Assets/Codes/OpenSkyNetwork.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System;
+using System.IO;
+
+public class OpenSkyNetwork : MonoBehaviour
+{
+    private String api = "https://opensky-network.org/api/flights/arrival?airport=EFHK&begin=1517227200&end=1517230800";
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetAirPlane();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private string GetAirPlane()
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format(api));
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string jsonResponse = reader.ReadToEnd();
+        Debug.Log(jsonResponse);
+        
+        return jsonResponse;
+    }
+}
