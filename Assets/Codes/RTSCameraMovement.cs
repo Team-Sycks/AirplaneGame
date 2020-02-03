d05992720bf53b4a8763ab720504c818009d3ac4
Sami Jaanu <jaanusa.sjrgmail.com>
2020-02-03 14:03:49 +0200

    kamera liikkuvuus

---
+++ Assets/Codes/RTSCameraMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class RTSCameraMovement : MonoBehaviour
+{
+    public float panSpeed = 100f;
+    public float panBorderThickness = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.height - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+}
