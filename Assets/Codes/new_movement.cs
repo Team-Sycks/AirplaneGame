40c9e133430e3ee58bf2f5b6f97e2da66314bcbe
tuoamas <tuomas.koskipahta@gmail.com>
2020-02-06 14:58:36 +0200

    Added new movement script

---
+++ Assets/Codes/new_movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class new_movement : MonoBehaviour
+{
+    public GameObject g;
+    Vector3[] points;
+    int index;
+    int i;
+    void Start()
+    {
+        g = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        g.transform.position = new Vector3(-5, 0, -3);
+       
+        points = new Vector3[4];
+        points[0] = new Vector3(3, 0, 3);
+        points[2] = new Vector3(-4, 0, -4);
+        points[1] = new Vector3(4, 0, 2);
+        points[3] = new Vector3(2, 0, -1);
+    }
+    void Update()
+    {
+        g.transform.position = Vector3.MoveTowards(g.transform.position,
+                                                          points[index], Time.deltaTime * 2);
+
+        // Looks at zero
+        g.transform.LookAt(Vector3.zero);
+
+
+        // Alla olevan Linen poistaminen --> Gameobject liikkuu vain ensimmäiseen pointtiin
+        if ( (g.transform.position - points[index]).sqrMagnitude < 0.01) { index++; if (index > points.Length - 1) { index = 0; } }
+    
+    // if (g.transform.position == points[index]) { Application.Quit(); }
+
+
+
+
+}
+}
