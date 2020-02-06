ac4027b3d3bdb515066627a9819df25cd47de369
sandels97 <santtu.hyvarinen@kuori.tech>
2020-02-06 14:19:25 +0200

    Updated PlaneLogic.cs and plane prefab

--- Assets/Codes/PlaneLogic.cs
+++ Assets/Codes/PlaneLogic.cs
@@ -7,6 +7,9 @@ public class PlaneLogic : MonoBehaviour
     public long departureTime;
     public long arrivalTime;
 
+    public float longitude;
+    public float latitude;
+
     public Vector3 departureCoordinates;
     public Vector3 arrivalCoordinates;
 
@@ -15,12 +18,17 @@ public class PlaneLogic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        float latScale = 2000 / 180.0f;  //latitude goes from 0(Npole) to 180(Spole)
+        float longiScale = 4000 / 360.0f; //longitude goes from 0 to 360 (Greenwich)
+        float z = latitude * latScale;
+        float x = longitude * longiScale;
+
+        this.transform.position = new Vector3(x, transform.position.y, z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
