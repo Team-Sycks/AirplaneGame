4122e3960120a521ad97e6101e57f108b411e819
Ilari Tuominen <ilari.tuominen@gmail.com>
2020-02-06 14:12:39 +0200

    Added LookAt script
    Added LookAt script

---
+++ Assets/Codes/LookAt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = rotation;
+        
+        transform.rotation = rotation * Quaternion.Euler(0, 360, 0);
+    }
+}
--- Assets/Codes/Moveplane.cs
+++ Assets/Codes/Moveplane.cs
@@ -1,12 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Vector3.MoveTowards example.
 
-public class object_move : MonoBehaviour
+// A cube can be moved around the world. It is kept inside a 1 unit by 1 unit
+// xz space. A small, long cylinder is created and positioned away from the center of
+// the 1x1 unit. The cylinder is moved between two locations. Each time the cylinder is
+// positioned the cube moves towards it. When the cube reaches the cylinder the cylinder
+// is re-positioned to the other location. The cube then changes direction and moves
+// towards the cylinder again.
+//
+// A floor object is created for you.
+//
+// To view this example, create a new 3d Project and create a Cube placed at
+// the origin. Create Example.cs and change the script code to that shown below.
+// Save the script and add to the Cube.
+//
+// Now run the example.
+
+public class Example : MonoBehaviour
 {
     // Adjust the speed for the application.
     public float speed = 1.0f;
-
+    public Transform cylinder;
     // The target (cylinder) position.
     private Transform target;
 
@@ -33,6 +49,7 @@ public class object_move : MonoBehaviour
         floor.transform.position = new Vector3(0.0f, -1.0f, 0.0f);
     }
 
+
     void Update()
     {
         // Move our position a step closer to the target.
@@ -45,6 +62,9 @@ public class object_move : MonoBehaviour
             // Swap the position of the cylinder.
             target.position *= -1.0f;
         }
-
+        Vector3 direction = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = rotation;
     }
-}
+    }
+
