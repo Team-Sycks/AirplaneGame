f04e71ef09d292cd59c71a4e4390f0a02b540d89
Ilari Tuominen <ilari.tuominen@gmail.com>
2020-02-10 12:06:00 +0200

    Added Movementplane script
    Plane moves and looks at target at all times

---
+++ Assets/Codes/MovementPlane.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlane : MonoBehaviour
+    
+{
+    public Transform target;
+    Vector3[] points;
+    int index;
+    int i;
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = new Vector3[4];
+        points[0] = new Vector3(8, 0, 5);
+        points[1] = new Vector3(4, 0, 9);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //Change Time.deltaTime *
+        transform.position = Vector3.MoveTowards(transform.position,
+                                                          target.position,speed * Time.deltaTime * 20);
+        Vector3 direction = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = rotation;
+
+        transform.rotation = rotation * Quaternion.Euler(0, 360, 0);
+    }
+}
